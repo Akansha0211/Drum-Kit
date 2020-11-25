34a3ef09caba4bf84b364ed9550e56ab24ca8a66
Akansha0211 <akansha.saxena0211@gmail.com>
2020-11-25 10:26:59 +0530

    Add EventListener to every button

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-document.querySelector("button").addEventListener("click",function(){
-    alert("I got clicked");
-});
+// document.querySelector("button").addEventListener("click",function(){
+//     alert("I got clicked");
+// });
 
-  
+// long way :naive approach
+// document.querySelectorAll("button")[0].addEventListener("click",function(){
+//     alert("I got clicked");
+// });
+
+// document.querySelectorAll("button")[1].addEventListener("click",function(){
+//     alert("I got clicked");
+// });
+
+for( var i = 0; i< document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    alert("I got clicked");
+    });
+}
